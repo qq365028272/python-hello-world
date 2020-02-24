e5717aaaf41648dbdc8932bbe7f1d82523a3e593
Jackson Wong <365028272@qq.com>
2020-02-24 19:27:01 +0000

    re_add week5 files

---
+++ week5/test2.py
@@ -0,0 +1,12 @@
+dic_lst = [ {"name":"Jack", "age":"20", "Sex": "Male"},\
+            {"name":"Alice", "age":"18", "Sex": "Female"},\
+            {"name":"Neck", "age":"11", "Sex": "Male"},\
+            {"name":"Lini", "age":"29", "Sex": "Female"},\
+            {"name":"Bob", "age":"30", "Sex": "Male"},\
+            {"name":"Eva", "age":"17", "Sex": "Female"},
+            {"name":"Sam", "age":"25", "Sex": "Male"}
+            ]
+i = 0
+while i < len(dic_lst):
+    print(dic_lst[i]["age"])
+    i += 1
